d977a0b0fe62c75aa0b0292e24fdfdd2dbc486d9
duc003972 <115331882+duc003972@users.noreply.github.com>
2022-10-09 10:57:04 +0700

    Hoang Bui Viet Duc - Em nop bai check point 1

---
+++ SS4/index.js
@@ -0,0 +1,54 @@
+/*
+    Câu 1: D
+    Câu 2: C
+    Câu 3: B
+    Câu 4: A
+    Câu 5: A
+    Câu 6: D
+    Câu 7: D
+    Câu 8: D
+*/
+// Câu 9:
+var m = [1, 2, 3, 4, 5, 6, "hh", 9, 80, 100]
+// var mFilter = m.filter(item => item= 1, 2, 3, 4, 5, 6)
+let mFilter = m.filter(item => item.valueOf(length) !== "hh");
+// let mFilter = m.filter((item) => {
+//         item.valueOf(length) !== "hh";
+//     })
+console.log(mFilter);
+
+
+// //  Câu 10:
+// var string = '"High knowledge, high return"'
+// var arr = string.toLowerCase().split('"')
+// console.log(arr);
+
+// // Cau 11:
+// class people{
+//     constructor(name, age, address){
+//         set(this.name = _name)
+//         set(this.age = _age)
+//         set(this.address = _address)
+//     }
+
+// }
+// class student extends people{
+//     constructor(_id, _math, _physical, _chemistry) {
+//     }
+// }
+// console.log(student);
+
+// Cau 14:
+let thiSinhDuThi = [
+    khoiA = {
+        monThi: ["Toan", "Li", "Hoa"],
+        
+    },
+    khoiB = {
+        monThi: ["Toan", "Hoa", "Sinh"]
+    },
+    khoiC = {
+        monThi: []
+    }
+]
+
